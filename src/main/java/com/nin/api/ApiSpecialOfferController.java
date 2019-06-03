5ab8d3545564e83fddf35c9d8546827a6969cdaf
ninhnam <ninhnam@192.168.0.103>
2019-06-04 05:46:41 +0700

    API SpecialOffer

---
+++ src/main/java/com/nin/api/ApiSpecialOfferController.java
@@ -0,0 +1,47 @@
+package com.nin.api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nin.model.SpecialOffer;
+import com.nin.service.SpecialOfferService;
+
+@RestController
+@RequestMapping("/api/v1/specialoffer")
+public class ApiSpecialOfferController {
+    private SpecialOfferService specialOfferService;
+
+    @Autowired
+    public ApiSpecialOfferController(SpecialOfferService specialOfferService) {
+        this.specialOfferService = specialOfferService;
+    }
+
+
+    @GetMapping()
+    public ResponseEntity<Map<String, Object>> listSpecialOffer() {
+        List<SpecialOffer> currentSpecialOffer = specialOfferService.findAllActive();
+        List<Map<String, Object>> result = new ArrayList<>();
+        for(SpecialOffer specialOffer : currentSpecialOffer) {
+            Map<String, Object> obj = new HashMap<>();
+            obj.put("id", specialOffer.getSpecialOfferId());
+            obj.put("image", specialOffer.getImage());
+            obj.put("webLink", specialOffer.getWebLink());
+            result.add(obj);
+        }
+        Map<String, Object> out = new HashMap<String, Object>() {{
+            put("data", result);
+            put("error", 0);
+
+        }};
+        return new ResponseEntity<>(out, HttpStatus.OK);
+    }
+}
